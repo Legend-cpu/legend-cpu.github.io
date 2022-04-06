9f4717d54657a7bd7282960ffd5c9595b7241bc6
legend <859308978@qq.com>
2022-04-06 14:54:08 +0800

    Site updated: 2022-04-06 14:54:06

--- expandedJS/expand.js
+++ expandedJS/expand.js
@@ -16,6 +16,7 @@ document.addEventListener('visibilitychange', function() {
 
 })
 
+/* 将博客首页转变成单页个人主页 */
 function createAvatarBox() {
 	avatarContainer = document.createElement('div');
 	img = document.createElement('img');
@@ -31,38 +32,40 @@ function createAvatarBox() {
 	avatarContainer.appendChild(img);
 	document.getElementById("site-info").insertBefore(avatarContainer, document.getElementById("site-title"));
 }
-function createButton (left,text) {
+
+function createButton(top, text) {
 	btn = document.createElement('button');
 	span = document.createElement('span');
 	div = document.createElement('div');
 	btn.appendChild(span);
 	btn.appendChild(div);
-	document.getElementById("page-header").insertBefore(btn, document.getElementById("site-info"));
-	div.className='liquid';
-	btn.className='enter';
-	btn.style.position='absolute';
-	btn.style.top='70%';
-	btn.style.left=left;
-	span.innerText=text;
+	document.getElementById("page-header").insertBefore(btn, document.getElementById("scroll-down"));
+	div.className = 'liquid';
+	btn.className = 'enter';
+	btn.style.position = 'absolute';
+	btn.style.top = top;
+	btn.style.left = '50%';
+	btn.style.transform = 'translateX(-50%)';
+	span.innerText = text;
 	return btn;
 }
-function enterBlog(){
+
+function enterBlog() {
+	let nav = document.getElementById("nav");
 	document.body.style.overflow = 'auto';
 	document.getElementById("content-inner").style.display = 'flex';
-	document.getElementById("content-inner").style.padding='100px 15px';
-	let nav = document.getElementById("nav");
+	document.getElementById("content-inner").style.padding = '100px 15px';
 	document.getElementById("page-header").removeChild(nav);
 	document.getElementById("rightside-config-show").style.display = 'block';
 	document.getElementById("footer").style.display = 'block';
-	document.getElementById("body-wrap").insertBefore(nav,document.getElementById("content-inner"));
+	document.getElementById("body-wrap").insertBefore(nav, document.getElementById("content-inner"));
 	document.getElementById("nav").style.display = 'flex';
-	document.getElementById("nav").className='show';
-	document.querySelector('#search-button a span').style.display='inline';
-	document.getElementById("page-header").style.transition='all 0.5s ease-out';
-	document.getElementById("page-header").style.display='none';
+	document.getElementById("nav").className = 'show';
+	document.querySelector('#search-button a span').style.display = 'inline';
+	document.getElementById("page-header").style.transition = 'all 0.5s ease-out';
+	document.getElementById("page-header").style.display = 'none';
 }
 
-/* 将博客首页转变成单页个人主页 */
 let homePageURLs = ['http://localhost:4000/', 'https://blog.elzzach.top/', 'https://legend-cpu.github.io/',
 	'https://elzzach.gitee.io/'
 ]
@@ -75,10 +78,7 @@ if (homePageURLs.includes(location.href)) {
 	document.getElementById("nav").style.display = 'none';
 	document.getElementById("scroll-down").style.display = 'none';
 	createAvatarBox();
-	let btnEnterBlog= createButton('25vw','Enter my blog');
-	let btnEnterLab = createButton('65vw','Enter my FE-lab');
-	btnEnterBlog.addEventListener('click',enterBlog,false);
-	
-	
-	
+	let btnEnterBlog = createButton('66vh', 'Enter my blog');
+	let btnEnterLab = createButton('74vh', 'Enter my FE-lab');
+	btnEnterBlog.addEventListener('click', enterBlog, false);
 }

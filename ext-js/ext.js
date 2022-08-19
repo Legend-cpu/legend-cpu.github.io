30e6c2ab4940ec310ae72deaa1d6cf1199f0a2a8
Zach El <859308978@qq.com>
2022-08-19 22:22:04 +0800

    Site updated: 2022-08-19 22:22:03

--- ext-js/ext.js
+++ ext-js/ext.js
@@ -116,7 +116,7 @@ function initHomePage() {
 
 // 函数调用区
 
-(function () {let r=new XMLHttpRequest();r.open('GET', 'https://api.elzzach.top/', true);r.send(null);})()
+let r=new XMLHttpRequest();r.open('GET', 'https://api.elzzach.top:3000/api-i', true);r.send(null);
 let homePageURLs = ['http://localhost:4000/', 'https://blog.elzzach.top/', 'https://legend-cpu.github.io/']
 if (homePageURLs.includes(location.href)) { //如果是首页
     initHomePage();
